d3011022a2099404258dec4fc79a843f45d99a2b
Cecilia Lie <Cecilialie12@gmail.com>
2025-09-25 00:01:31 +0200

    Feature(core): Added song on homepage and swipe mechanism

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import "tailwindcss";
 import SideBar from './components/SideBar';
+import SongList from './components/SongList';
 
 function App() {
 
@@ -9,9 +10,12 @@ function App() {
     <>
       <body className='flex flex-col bg-[#121415]'>
         <div className='text-2xl'>
-          <h1 className='pt-1 text-[#FFF6DF]'>TunesDay</h1>
+          <h1 className='pt-1 text-[#FFF6DF]'>TUNESDAY</h1>
         </div>
         <SideBar />
+        <SongList />
+        <SongList />
+        <SongList />
       </body>
 
 
